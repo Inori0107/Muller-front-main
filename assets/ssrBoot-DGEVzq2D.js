58fde2f57b4ac79ee77f5404ee272026ec8eecf5
Inori0107 <163396929+Inori0107@users.noreply.github.com>
2024-10-08 18:54:23 +0000

    Deploying to gh-pages from @ Inori0107/Muller-front-main@7b941222225e2edaa1b3df72938c2a4337a66cc8 🚀

---
+++ assets/ssrBoot-DGEVzq2D.js
@@ -0,0 +1,1 @@
+import{H as t,a0 as e,J as s,bf as n}from"./index-KgvYbjNy.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
